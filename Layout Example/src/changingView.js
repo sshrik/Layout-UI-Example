2efd095e52b08760be4d28b13a53080d49aa304d
sshrik <chj878194@naver.com>
2019-07-15 16:06:14 +0900

    Now use toggleClass funtion.

--- Layout Example/src/changingView.js
+++ Layout Example/src/changingView.js
@@ -1,35 +1,4 @@
 (function($) {
-    // For change view class add.
-    function modifyView(index, idList) {
-        /*  Need to modify to show classList[index].
-            i : Integer value of classList`s index for add class "viewBlock".
-            idList : String value list for view space id list.  */
-
-       console.log(idList[index]);
-        for(var i = 0; i < idList.length; i++) {
-            // If i == index, add viewBlock and remove viewNone.
-            if(i == index) {
-                if($("#" + idList[i]).hasClass("viewNone")) {
-                    $("#" + idList[i]).removeClass("viewNone")
-                }
-                if(!$("#" + idList[i]).hasClass("viewBlock")) {
-                    $("#" + idList[i]).addClass("viewBlock")
-                }
-                console.log($("#" + idList[i]).attr('class'));
-            }
-            // else, add viewNone and remove viewBlock.
-            else {
-                if(!$("#" + idList[i]).hasClass("viewNone")) {
-                    $("#" + idList[i]).addClass("viewNone")
-                }
-                if($("#" + idList[i]).hasClass("viewBlock")) {
-                    $("#" + idList[i]).removeClass("viewBlock")
-                }
-                console.log($("#" + idList[i]).attr('class'));
-            }
-        }
-    };
-
     $.fn.changingView = function(options)    {
         /*  options = { btnList,  idList }; 
             Each list are string values.    */
@@ -39,10 +8,16 @@
             idList: []
         }, options);
 
+        var lastClicked = -1;
+
+
         // Add each button to add modifing view function.
         for(var i = 0; i < settings.btnList.length; i++)    {
+            var nowClicked = settings.btnList.indexOf($(this).attr('id'));
             $('#' + settings.btnList[i]).on("click", function() {
-                modifyView(settings.btnList.indexOf($(this).attr('id')), settings.idList);
+                $('#' + settings.idList[lastClicked]).toggleClass('viewNone');
+                $('#' + settings.idList[nowClicked]).toggleClass('viewNone');
+                lastClicked = nowClicked;
             });
         }
 
